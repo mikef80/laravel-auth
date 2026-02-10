1ea151f10687392d323c6c5f1b8e41b4127105de
Mike <mike@mike-francis.org>
2026-02-10 22:41:51 +0000

    tidied up api.php, removed unused routes

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Validator;
 
-Route::get('/user', function (Request $request) {
-  return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
